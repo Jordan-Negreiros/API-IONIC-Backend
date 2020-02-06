f08a5015afbf0195c7f7710064ab5451597394ce
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-05 22:27:30 -0300

    Estado e Cidade

---
+++ src/main/java/com/jordan/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.jordan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jordan.cursomc.domain.Cidade;
+
+public interface CidadeRepository  extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/jordan/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.jordan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jordan.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
