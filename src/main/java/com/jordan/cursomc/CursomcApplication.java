bb5d6c5e02fb70d85bf9f9aa78566e2861d8c92a
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-06 13:26:20 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/jordan/cursomc/CursomcApplication.java
+++ src/main/java/com/jordan/cursomc/CursomcApplication.java
@@ -1,25 +1,16 @@
 package com.jordan.cursomc;
 
-import java.util.Arrays;
-
+import com.jordan.cursomc.domain.*;
+import com.jordan.cursomc.domain.enums.EstadoPagamento;
+import com.jordan.cursomc.domain.enums.TipoCliente;
+import com.jordan.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.jordan.cursomc.domain.Categoria;
-import com.jordan.cursomc.domain.Cidade;
-import com.jordan.cursomc.domain.Cliente;
-import com.jordan.cursomc.domain.Endereco;
-import com.jordan.cursomc.domain.Estado;
-import com.jordan.cursomc.domain.Produto;
-import com.jordan.cursomc.domain.enums.TipoCliente;
-import com.jordan.cursomc.repositories.CategoriaRepository;
-import com.jordan.cursomc.repositories.CidadeRepository;
-import com.jordan.cursomc.repositories.ClienteRepository;
-import com.jordan.cursomc.repositories.EnderecoRepository;
-import com.jordan.cursomc.repositories.EstadoRepository;
-import com.jordan.cursomc.repositories.ProdutoRepository;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
@@ -36,10 +27,15 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -82,6 +78,23 @@ public class CursomcApplication implements CommandLineRunner{
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
 	}
 
 }
