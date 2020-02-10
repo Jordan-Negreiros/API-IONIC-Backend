730edcc6257d836be635855f29c1594519270f5e
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-10 12:44:00 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/jordan/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/jordan/cursomc/resource/CategoriaResource.java
@@ -2,8 +2,9 @@ package com.jordan.cursomc.resource;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -36,17 +37,19 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insertEntity(@RequestBody Categoria obj) {
+	public ResponseEntity<Void> insertEntity(@Valid @RequestBody CategoriaDTO objDto) {
 		
+		Categoria obj = service.fromDTO(objDto);
 		obj = service.insertCategoria(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objDto.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> updateEntity(@RequestBody Categoria obj, @PathVariable Integer id) {
+	public ResponseEntity<Void> updateEntity(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
 		
+		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.updateCategoria(obj);
 		
--- src/main/java/com/jordan/cursomc/services/CategoriaService.java
+++ src/main/java/com/jordan/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jordan.cursomc.domain.Categoria;
+import com.jordan.cursomc.dto.CategoriaDTO;
 import com.jordan.cursomc.exceptions.DataIntegrityException;
 import com.jordan.cursomc.exceptions.ObjectNotFoundException;
 import com.jordan.cursomc.repositories.CategoriaRepository;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
