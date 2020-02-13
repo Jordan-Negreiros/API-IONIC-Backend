005631d4ff7b49b38a6bc23d6d68286c297b00a6
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-13 16:35:26 -0300

    Validação customizada: email nao repetido na atualizacao de Cliente

---
+++ src/main/java/com/jordan/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.jordan.cursomc.services.validation;
+
+import com.jordan.cursomc.domain.Cliente;
+import com.jordan.cursomc.domain.enums.TipoCliente;
+import com.jordan.cursomc.dto.ClienteDTO;
+import com.jordan.cursomc.repositories.ClienteRepository;
+import com.jordan.cursomc.resource.exception.FieldMessage;
+import com.jordan.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Override
+    public void initialize(ClienteUpdate ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Cliente aux = repository.findByEmail(objDto.getEmail());
+        if (aux != null && !aux.getId().equals(uriId)) {
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
