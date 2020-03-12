773ba7c02547a902257d923440875078aee3846d
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-03-12 15:09:17 -0300

    Testando envio de arquivo ao AmazonS3

--- src/main/java/com/jordan/cursomc/CursomcApplication.java
+++ src/main/java/com/jordan/cursomc/CursomcApplication.java
@@ -1,46 +1,24 @@
 package com.jordan.cursomc;
 
-import com.jordan.cursomc.domain.*;
-import com.jordan.cursomc.domain.enums.EstadoPagamento;
-import com.jordan.cursomc.domain.enums.TipoCliente;
-import com.jordan.cursomc.repositories.*;
+import com.jordan.cursomc.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	@Autowired
-	private EstadoRepository estadoRepository;
-	@Autowired
-	private CidadeRepository cidadeRepository;
-	@Autowired 
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("F:\\Curso Spring Boot\\testeamazons3.jpg");
 	}
 
 }
