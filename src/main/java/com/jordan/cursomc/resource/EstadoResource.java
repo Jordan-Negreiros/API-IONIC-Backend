0bceaae8b8dc43bf6b0a34f862fe3a6167bf0a27
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-03-16 16:20:15 -0300

    Novo Repositório

---
+++ src/main/java/com/jordan/cursomc/resource/EstadoResource.java
@@ -0,0 +1,29 @@
+package com.jordan.cursomc.resource;
+
+import com.jordan.cursomc.domain.Estado;
+import com.jordan.cursomc.dto.EstadoDTO;
+import com.jordan.cursomc.services.EstadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/estados")
+public class EstadoResource {
+
+    @Autowired
+    private EstadoService estadoService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<EstadoDTO>> findAll() {
+        List<Estado> list = estadoService.findAll();
+        List<EstadoDTO> listDto = list.stream().map(obj -> new EstadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
+}
